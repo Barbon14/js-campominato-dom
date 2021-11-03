c0a466fdfe1b21874e68929b1ea44289bd5228bb
BarbonFisso <barbieri.mat@hotmail.it>
2021-11-03 13:54:54 +0100

    aggiunto creazione array bombe

--- js/script.js
+++ js/script.js
@@ -3,7 +3,17 @@
 // con difficoltà 1 => tra 1 e 100
 // con difficoltà 2 => tra 1 e 81
 // con difficoltà 3 => tra 1 e 49
-// Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro(o simili, l’importante è dare all’utente il feedback che ha scoperto una casella che rimarrà scoperta, con il numero relativo).
+
+// Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe.
+// I numeri nella lista delle bombe non possono essere duplicati.
+
+// In seguito l’utente clicca su ogni cella:
+// se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina,
+// altrimenti la cella cliccata si colora di azzurro e l’utente può continuare a cliccare sulle altre celle.
+// (come detto sull’effetiva interfaccia fate voi, non ci son specifiche vincolanti, ma partite semplici)
+
+// La partita termina quando il giocatore clicca su una bomba
+// o raggiunge il numero massimo possibile di numeri consentiti.
 
 // chiedo all'utente di scegliere la difficoltà
 let difficultySet = parseInt(prompt(`Seleziona il livello di difficoltà:
@@ -17,12 +27,32 @@ console.log(difficultySet);
 // - difficulty == 2 , genero 81 quadrati
 // - difficulty == 3 , genero 49 quadrati
 if (difficultySet === 1) {
-    generateGrid(100, "size-lvl-1")
+    squareNum = 100;
+    squareSize = "size-lvl-1";
 } else if (difficultySet === 2) {
-    generateGrid(81, "size-lvl-2")
+    squareNum = 81;
+    squareSize = "size-lvl-2";
 } else if (difficultySet === 3) {
-    generateGrid(49, "size-lvl-3")
+    squareNum = 49;
+    squareSize = "size-lvl-3";
 }
+
+generateGrid(squareNum, squareSize);
+
+const bombs = [];
+
+// genero numeri per riempire l'array fino a quando non ha 50 elementi
+while (bombs.length < 16) {
+    let num = ramdomNum(1, squareNum);
+
+    // controllo che il numero non sia già presente all'interno dell'array
+    if (bombs.includes(num) === false) {
+        bombs.push(num);
+    }
+}
+
+console.log(bombs);
+
 
 // funzioni
 
@@ -53,4 +83,10 @@ function generateGrid(squareNum, squareSize) {
 
         squareCont.append(node);
     }
+}
+
+// genera numero casuale
+function ramdomNum(min, max) {
+    let rNum = Math.floor(Math.random() * max) + min;
+    return rNum;
 }
